725f60357a89215f5d8c5b9e829b5fdb65d53a26
kagwi <kagwiandrew@gmail.com>
2020-07-22 19:17:14 +0300

    done opening full pagination

--- src/data-source/kenyatalk.js
+++ src/data-source/kenyatalk.js
@@ -11,19 +11,22 @@ module.exports = function createKijijiDataSource(browser) {
     // eslint-disable-next-line no-undef
     async function storyLinks() {
       //
-      await page.waitFor(1000);
+      await page.waitFor(10000);
       // this is a paginating button that should be clicked through for scraping
-      const allBtns = await page.waitForSelector('li.pageNav-page--skipEnd >a'); //document.querySelector('span.inputGroup-text');
+      const allBtns = await page.waitForSelector('li.pageNav-page--skipEnd >a'); // document.querySelector('span.inputGroup-text');
+      const popBtn = await page.waitForSelector('button.js-pageJumpGo');
       // const nextBtn = 'li.pageNav-page >a';
 
 
       if (allBtns) await allBtns.click();
-      console.log('DONE CLICK', allBtns);
+      await page.waitFor(10000);
+      if (popBtn) await popBtn.click();
+      //  console.log('DONE CLICK', popBtn);
 
       await page.waitFor(10000);
 
       const pagedEls = Array.from(await page.$$('li.pageNav-page >a'));
-      console.log('NOW SEE THE BUTTONS LIST', pagedEls);
+      console.log('NOW SEE THE BUTTONS LIST', pagedEls.length);
       const hekayas = await page.evaluate(() => Array.from(document.querySelectorAll('div.structItem'))
         .map(eventRowEl => ({
           storyLink: eventRowEl.querySelector('div.structItem-title >a')
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ const puppeteer = require('puppeteer');
 const createDatasource = require('./data-source');
 
 module.exports = async function scrapeKijiji() {
-  const browser = await pupeteer.launch({
-    headless: 'false',
+  const browser = await puppeteer.launch({
+    headless: false,
     timeout: process.env.DEFAULT_TIMEOUT ? parseInt(process.env.DEFAULT_TIMEOUT, 10) : 120000,
     defaultViewport: null,
     args: ['--window-size=1920,1080'],
